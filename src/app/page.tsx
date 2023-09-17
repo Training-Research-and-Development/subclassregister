44e6dd830364330cf2753ef740054d4398f58652
DESKTOP-18D80L2\mokot <mokotoe_kiku@hotmail.com>
2023-09-17 20:16:19 +0700

    test checkbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,124 @@
-import Image from 'next/image'
+"use client";
+import React ,{ useState, useEffect, useCallback, ChangeEvent  } from 'react';
+import {
+  Box,
+  Grid,
+  Checkbox,
+  FormControlLabel,
+  List,
+  Divider,
+} from "@mui/material";
+import Image from "next/image";
+
+interface classData {
+  ID: number;
+  RoomName: string;
+  ClassSubject: string;
+  learning_day: string;
+  learning_time: string;
+  instructor: string;
+}
+
+interface classList {
+  trdclass: Array<classData>;
+}
+
+let data = {
+  trdclass: [
+    {
+      ID: 1,
+      RoomName: "605 Tower FL.4",
+      ClassSubject: "พระราชกรณียกิจที่สำคัญของแต่ละรัชกาลในราชวงศ์จักรี",
+      learning_day: "2023-10-09",
+      learning_time: "09.00 น. - 12.00 น.",
+      instructor: "ศ.ดร.ม.ร.ว.สุริยวุฒิ สุขสวัสดิ์",
+    },
+    {
+      ID: 2,
+      RoomName: "605 Tower FL.4",
+      ClassSubject:
+        "พระราชพิธีบรมราชาภิเษก, เกร็ดความรู้ทั่วไป (เครื่องราชอิสริยาภรณ์, ธงพระอิสริยยศ ฯลฯ",
+      learning_day: "2023-10-09",
+      learning_time: "13.00 น. - 16.00 น.",
+      instructor: "ศ.ดร.ม.ร.ว.สุริยวุฒิ สุขสวัสดิ์",
+    },
+    {
+      ID: 3,
+      RoomName: "605 Tower FL.4",
+      ClassSubject: "บทบาทของสถาบันพระมหากษัตริย์ไทย ในการพัฒนาประเทศ",
+      learning_day: "0000-00-00",
+      learning_time: "13.00 น. - 16.00 น.",
+      instructor: "dsaf",
+    },
+  ],
+};
 
 export default function Home() {
+  const [seminarclass, setSeminarClass] = useState<Array<number>>([])
+
+  const onFieldChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.checked);
+    let x = seminarclass
+    const index = x.indexOf(parseInt(event.target.value))
+                          if (index > -1) { // only splice array when item is found
+                            x.splice(index, 1); // 2nd parameter means remove one item only
+                          }
+    setSeminarClass([...x, parseInt(event.target.value)])
+  }
+
+  
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    setSeminarClass(seminarclass)
+    
+  },[seminarclass]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <div className="bg-hero bg-contain bg-no-repeat w-96 h-56"></div>
+      <p className="text-2xl">เลือกวิชาที่ต้องการลงทะเบียน</p>
+      <Box sx={{ flexGrow: 1, mt: 4 }}>
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          sx={{ ml: 2, pr: 2 }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {data.trdclass.map((item, index) => {
+            return (
+              <List key={index}>
+                <FormControlLabel
+                  value={item.ID}
+                  control={
+                    <Checkbox 
+                      checked={seminarclass.includes(item.ID)}
+                      onChange={onFieldChange}
+                      // onChange={()=> {
+                      //   let x = seminarclass
+                      //   if(!x.includes(item.ID)){
+                      //     x.push(item.ID)
+                      //     setSeminarClass(x)  
+                      //   }else {
+                      //     const index = x.indexOf(item.ID)
+                      //     if (index > -1) { // only splice array when item is found
+                      //       x.splice(index, 1); // 2nd parameter means remove one item only
+                      //     }
+                      //     setSeminarClass(x)
+                      //   }
+                      // }}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                    />
+                  }
+                  label={`เรื่อง ${item.ClassSubject} วิทยากร: ${item.instructor} วัน: ${item.learning_day} เวลา: ${item.learning_time} สถานที่: ${item.RoomName}`}
+                  //
+                />
+                <Divider variant="middle" component="li" />
+              </List>
+            );
+          })}
+        </Grid>
+      </Box>
+    </div>
+  );
 }
